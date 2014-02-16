1b9100341193faf0a9026b37767dfa2149144eef
ROFLCopter64bit <ROFLCopter64bit@gmail.com>
2014-02-16 18:50:19 +1100

    World and WorldStart Packet implementation begins

---
+++ Packets/WorldStart.vb
@@ -0,0 +1,12 @@
+﻿Public Class WorldStartPacket
+    Inherits Packet
+    Public Overrides Property OPCode As Byte = &HC
+
+    Sub New(ByVal World As World)
+
+    End Sub
+
+    Public Overrides Function GetByteArray() As Byte()
+
+    End Function
+End Class
---
+++ World.vb
@@ -0,0 +1,7 @@
+﻿Public Class World
+    Public Property RawBytes As Byte()
+    Sub New(ByVal Bytes() As Byte)
+        RawBytes = Bytes
+        'No point writing anything else here for now
+    End Sub
+End Class
