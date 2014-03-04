6a1fce79a6c70c9f526344df58019fafd0eb1970
ROFLCopter64bit <ROFLCopter64bit@gmail.com>
2014-03-04 17:57:39 +1100

    ClientConnect Implementation

--- Packets/ClientConnect.cs
+++ Packets/ClientConnect.cs
@@ -18,17 +18,14 @@ public class ClientConnectPacket : Packet
 
 	public ClientConnectPacket(byte[] Bytes) : base(Bytes)
 	{
-		Payload = Misc.Deflate(Payload);
+        byte[] lel = Payload;
 		AssetDigest = ReadByteArray();
-		dynamic b = ReadBytes(100);
-		//Dim c = Payload
-		//Dim cc = System.Text.Encoding.UTF8.GetChars(Payload)
-		//GetByteArray() 'lel, this is placeholder for claim
-		//Dim u = ReadByte()
-		//If u = 1 Then UUID = ReadBytes(16)
-		//Name = ReadString()
-		//Species = ReadString()
-		//There's no point in continuing here, but i'll finish this for another day
+		Claim = ReadVariant();
+        if (ReadBoolean()) UUID = ReadBytes(16);
+        Name = ReadString();
+        Species = ReadString();
+        Shipworld = ReadByteArray();
+        Account = ReadString();
 	}
 
 	public override byte[] GetByteArray()
